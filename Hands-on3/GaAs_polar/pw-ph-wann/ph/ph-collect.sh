665fe2a6beeffcbe3fe2094c35abc97952c7d309
Khoa B Le <85775106+lbkhoa2797@users.noreply.github.com>
2025-09-16 13:48:53 -0700

    Add files via upload

---
+++ Hands-on3/GaAs_polar/pw-ph-wann/ph/ph-collect.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PREFIX='gaas'
+
+#should be in the work directory of PHonon calculation
+echo `pwd`
+mkdir -p save
+mkdir -p save/${PREFIX}.phsave
+
+DIR="./tmp/_ph0"
+echo $DIR
+
+#copy prefix.phsave
+cp ${DIR}/${PREFIX}.phsave/* save/${PREFIX}.phsave/
+
+#copy dyn files
+cp ${PREFIX}.dyn* save/
+
+# For NQ=1
+NQ=1
+cp ${DIR}/${PREFIX}.dvscf save/${PREFIX}.dvscf_q${NQ}
+
+# Other Q's
+for ((NQ=2; NQ<=3; NQ++ ))
+do
+   cp ${DIR}/${PREFIX}.q_${NQ}/${PREFIX}.dvscf save/${PREFIX}.dvscf_q${NQ}
+done
+
