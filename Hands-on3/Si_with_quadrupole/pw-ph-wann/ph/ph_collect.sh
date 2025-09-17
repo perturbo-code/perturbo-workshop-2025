745a6399000d9f952bd5123168a8d0fd9a457f04
Khoa B Le <85775106+lbkhoa2797@users.noreply.github.com>
2025-09-17 14:13:37 -0700

    Update ph_collect.sh

--- Hands-on3/Si_with_quadrupole/pw-ph-wann/ph/ph_collect.sh
+++ Hands-on3/Si_with_quadrupole/pw-ph-wann/ph/ph_collect.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-PREFIX='cri3'
+PREFIX='si'
 
 #should be in the work directory of PHonon calculation
 echo `pwd`
 mkdir -p save
 mkdir -p save/${PREFIX}.phsave
 
-for ((NQ=1; NQ<=19; NQ++ ))
+DIR="./tmp/_ph0"
+echo $DIR
+
+#copy prefix.phsave
+cp ${DIR}/${PREFIX}.phsave/* save/${PREFIX}.phsave/
+
+#copy dyn files
+cp ${PREFIX}.dyn* save/
+
+# For NQ=1
+NQ=1
+cp ${DIR}/${PREFIX}.dvscf save/${PREFIX}.dvscf_q${NQ}
+
+# Other Q's
+for ((NQ=2; NQ<=3; NQ++ ))
 do
-   DIR="ph-$NQ/tmp/_ph0"
-   echo $DIR
-   #copy prefix.phsave
-   cp ${DIR}/${PREFIX}.phsave/* save/${PREFIX}.phsave/
-   #copy dyn files
-   cp ph-${NQ}/${PREFIX}.dyn* save/
-   #copy dvscf files
-   cp ${DIR}/${PREFIX}.q_${NQ}/${PREFIX}.dvscf1 save/${PREFIX}.dvscf_q${NQ}
+   cp ${DIR}/${PREFIX}.q_${NQ}/${PREFIX}.dvscf save/${PREFIX}.dvscf_q${NQ}
 done
